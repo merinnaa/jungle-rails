fd292424b8c978d404fe8841aabb4dd41d03c864
luwam <lwamyohannes2018@gmail.com>
2023-11-28 23:25:05 -0700

    About Page added

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,6 @@
+class AboutController < ApplicationController
+  def index
+    # By default, it will look for a view template with the same name as the action (index.html.erb)
+    # in the app/views/about directory.
+  end
+end
